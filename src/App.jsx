96fd1b2ee4a860a1b82f444f498bbe2740f6b393
Owen Lee <lee@Owen-Lee-Rogers.local>
2022-12-03 16:44:27 +0300

    Done with the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './Components/Nav';
 
 function App() {
   return (
     <div>
+      <Nav />
 
-      <h1 className="text-4xl underline italic">App JSX</h1>
       
     </div>
   );
